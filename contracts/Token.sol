d31ba26108957c6ca439e445648d304024baf17e
JodsMigel <107489254+JodsMigel@users.noreply.github.com>
2022-10-26 15:34:11 +0900

    fix

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,12 +7,24 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
 contract SuperYachtCoin is  ERC20, ERC20Burnable, Pausable, Ownable  {
 
     constructor() ERC20("Super Yacht Coin", "SYC") {}
 
+    bool public vestingAddressLocked;
+
+    address public vestingAddress;
+
+    function addVesingAddress(address _vestingAddress) public onlyOwner {
+        require(!vestingAddressLocked, "vesting address is already set");
+        vestingAddressLocked = true;
+        vestingAddress = _vestingAddress;
+    }
+
     // @dev                                 only vesting contract can call this method
-    function mint(address _to, uint _amount) internal {
+    function mint(address _to, uint _amount) public {
+        require(msg.sender == vestingAddress, "Only vesting contract can call this method");
         _mint(_to, _amount);
     }
 
